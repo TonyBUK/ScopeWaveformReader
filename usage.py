b1bc92b34e3965dfd7084bb7310a9d1a5d349b0d
TonyBUK <megatony@gmail.com>
2023-05-18 17:01:16 +0100

    Beefed up usage.

--- usage.py
+++ usage.py
@@ -1,3 +1,13 @@
+###############################################################################
+# Method 1 - Using the Scope Reader Plugin System
+#
+# This example uses the scopereader class.  This will effectively take the
+# file, and have each of the plugins try to identify whether they can handle
+# the file type.  This is probably the way I expect most people to use this,
+# as when working correctly, you can basically throw whatever file you want at
+# it, and with a bit of luck, it'll spit out a decoded Waveform for you.
+###############################################################################
+
 import scopereader
 
 # The Waveform Class can take either the file name, or the file contents as a byte buffer/list/any other iteratable type.
@@ -22,7 +32,14 @@ with scopereader.Reader("sample.wfm") as waveform_reader :
 
 #end
 
-# Or... you can ignore the scope reader entirely and go straight to the waveform reader of choice.
+###############################################################################
+# Method 2 - Bypassing the Scope Reader Plugin System
+#
+# Each plugin module has the exact same interface as the Scope Reader itself,
+# so if the Scope Reader class is misbehaving, or you already know exactly
+# which reader you need, you can just access it directly.
+###############################################################################
+
 import plugins.TekTronixWFM_Reader
 
 with plugins.TekTronixWFM_Reader.Reader("sample.wfm") as waveform_reader :
@@ -45,3 +62,38 @@ with plugins.TekTronixWFM_Reader.Reader("sample.wfm") as waveform_reader :
     #end
 
 #end
+
+###############################################################################
+# Full Usage - Note: This is both for the scope reader, and any of the
+#                    plugins.
+#
+# Parameters:
+#
+# Required: kInput          : File Name OR File Buffer
+#
+# Optional: kErrorHandling  : This tells the plugins how to handle encountered
+#                             errors, there are 4 levels:
+#
+#                             ErrorLevelHandling.NONE   - No Warnings or Errors
+#                             ErrorLevelHandling.INFO   - Warnings as Warnings
+#                                                         Errors as Warnings
+#                             ErrorLevelHandling.NORMAL - Warnings as Warnings
+#                                                         Errors as Errors
+#                             ErrorLevelHandling.ALL    - Warnings as Errors
+#                                                         Errors as Errors
+#
+#                             A warning will just output to stderror the error
+#                             message, whereas an error will raise an
+#                             exception.
+#
+# TODO: Note: This last parameter is more experimental for now, so consider it
+#             experimental.  It's only required for the CSV plugin for now.
+#             What I dislike is that it's contrary to the aim of the scope
+#             reader class, which tries to provide a normalised interface,
+#             whereas this requires you to know exactly which plugin you're
+#             using.
+#
+# Optional: kPluginOptions  : This dictionary will be specific per plugin an
+#                             allows customisation specific to that module.
+#                             
+###############################################################################
